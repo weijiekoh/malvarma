9ef8cd3aff3361ac45e4c63115a6f00b7ac5d7a6
Koh Wei Jie <contact@kohweijie.com>
2018-01-11 21:59:01 +0800

    fixed run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,4 +3,4 @@
 qemu-system-arm -kernel raspberry/kernel-qemu-4.4.bin -cpu arm1176 -m 256 -M \
     versatilepb -serial stdio \
     -append "root=/dev/sda2 rootfstype=ext4 rw loglevel=3 logo.nologo quiet" \
-    -drive format=raw,file=build/malvarma-0.1-alpha.img -no-reboot 
+    -drive format=raw,file=build/malvarma-0.1.1-alpha.img -no-reboot 

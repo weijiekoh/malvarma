717c01c1b47f6bda23591f6b6117d20f81cef4a4
Koh Wei Jie <contact@kohweijie.com>
2018-01-11 21:48:34 +0800

    Fixed bug in download_raspbian.sh

--- download_raspbian.sh
+++ download_raspbian.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 echo "Downloading Raspbian Lite..."
-axel -a https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-12-01/2017-11-29-raspbian-stretch-lite.zip -o \
-    raspberry/2017-11-29-raspbian-stretch-lite.zip
+axel -a https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-12-01/2017-11-29-raspbian-stretch-lite.zip \
+    -o 2017-11-29-raspbian-stretch-lite.zip
 
 wget -nv https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-12-01/2017-11-29-raspbian-stretch-lite.zip.sha256 \
-    -O raspberry/raspbian_checksum
+    -O raspbian_checksum
 
 echo "Verifying checksums..."
-sha256sum -c raspberry/raspbian_checksum
+
+sha256sum -c raspbian_checksum
+mv 2017-11-29-raspbian-stretch-lite.zip raspbian_checksum raspberry/
 
 echo "Unzipping Raspbian..."
 cd raspberry
